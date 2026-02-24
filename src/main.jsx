1f18aaa45dfa7b982ace0ccf88b1b8af6accdb9a
parano1aa <amaro.aranedao@gmail.com>
2026-02-23 21:17:28 -0300

    Actualizando a Tailwind v4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- ESTA LÍNEA ES VITAL
+import './index.css' // <-- ESTA LÍNEA ES VITAL PARA QUE HAYA DISEÑO
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/KodePortal//', // <-- AÑADE ESTA LÍNEA (El nombre de tu repo entre barras)
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/KodePortal/', 
 })
